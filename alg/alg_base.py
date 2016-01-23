d173a669d0c875f9be4bc734be27e2eed9573202
Jan Lindroos <jan.lindroos@lindata.net>
2016-01-23 15:53:54 +0100

    Completed fixes to alg_base and implementation of mc erros

--- alg/alg_base.py
+++ alg/alg_base.py
@@ -8,84 +8,59 @@ algorithm
 """
 
 import scipy as sp
-
+import lib.stat_lib as stat
+#mu=sum(w*x)/sum(w)=(mu_1*sum(w_1)+mu_2*sum(mu_2))/sum(w)
 #State of the scan
 class state(object):
     
     def __init__(self,rank,opt,model):
         #General info needed by state
         self.param_names=model.param_names#ordered names of parameteres
-        self.batch_size=opt.batch_size
         self.sample_size=opt.sample_size#Final sample size requested
         
         #Properties of state       
-        self.continue_sampling=True
+        self.continue_sampling=True#Global sampling state
         self.lnP_max=-sp.inf#Empty arrays giving the best fit for each chain
         self.size=0#Number of likelihood evaluations
-        self.accepted=0#Number of accepted models
+        self.accept=0#Number of accepted models
         self.mean=dict([(key,sp.nan) for key in self.params_names])#Global mean
-        self.mc_error=dict([(key,sp.nan) for key in self.params_names])#Standard deviation between chain means
-        self.weight=0#Sum of all weights
+        self.mc_error=dict([(key,sp.nan) for key in self.params_names])#Standard mc error
         
         #Properties of chains
-        self.chains['continue_sampling']=sp.ones(opt.chains)#Status of the different chains
-        self.chains['lnP_max']=-sp.inf*sp.ones(opt.chains)#chain best fit likelihoods
-        self.chains['params_bf']=[dict([(key,sp.nan) for key in self.params_names]) for i in range(opt.chains)]#chain best fit models
+        self.chains['continue_sampling']=sp.ones(opt.chains)#Sampling state of chains
+        self.chains['size']=sp.zeros(opt.chains)#Number of likelihood evaluations by chain
+        self.chains['accept']=sp.zeros(opt.chains)#Number of accepted points by chain
         self.chains['mean']=dict([(key,sp.nan*sp.ones(opt.chains)) for key in self.params_names])#total mean for each chain
         self.chains['mc_error']=dict([(key,sp.nan*sp.ones(opt.chains)) for key in self.params_names])#batch means estimated error http://arxiv.org/abs/math/0601446
-        self.chains['weight']=sp.zeros(opt.chains)#total weight of chains
-        self.chains['size']=sp.zeros(opt.chains)#Number of likelihood evaluations
-        self.chains['accept']=sp.zeros(opt.chains)#Number of accepted points in chain
-        
-            
-    def update_stats(self):
-        self.accepted=sp.sum(self.chain_accept)
-        self.sample_size=sp.sum(self.chain_sizes)
-        #Update means and errors if any chain contains acceptable points
-        #print 'pre state:',self.mean
-        if float(sp.sum(self.chain_weights))>0:
-            #Total mean
-            #for i in range(len(self.chain_weights)):
-                
-                #print self.chain_weights[i]*self.chain_means[i]
-            self.mean=sp.sum([self.chain_weights[i]*self.chain_means[i] for i in range(len(self.chain_weights)) if not sp.isnan(self.chain_means[i]).all()],0)/float(sp.sum(self.chain_weights))               
-            #Total error (assumes no correlation at present)
-            if sp.greater(self.chain_sizes,self.batch_size).any():
-                w=sp.array(self.chain_weights)/float(sp.sum(self.chain_weights))        
-                self.mean_error=sp.sqrt(sp.sum([w[i]**2*self.chain_mean_errors[i]**2 for i in range(len(w)) if self.chain_sizes[i]>self.batch_size],0))        
-
-        #print 'post state:',self.mean    
+        self.chains['weight']=dict([(key,sp.nan*sp.ones(opt.chains)) for key in self.params_names])#total weights of chains
+        self.chains['lnP_bf']=-sp.inf*sp.ones(opt.chains)#chain best fit likelihoods
+        self.chains['params_bf']=[dict([(key,sp.nan) for key in self.params_names]) for i in range(opt.chains)]#chain best fit models
+        self.chain_keys=self.chains.keys()#List of chain data to be sent to state
     
-    #Update states based on updates from chains
-    def update(self,updates):
-        #Do manipulation independent of MPI/mp
-        #Update the global state based on the info in local states
-        #................
-        #Quit sampling if total size exceeds specified sample size
-        for chain in updates:
-            self.chains['continue_sampling'][chain.rank]=chain.continue_sampling
-            self.chains['size'][chain.rank]=chain.size
-            self.chains['accept'][chain.rank]=chain.accepted
-            self.chains['mean'][chain.rank]=chain.mean
-            self.chains['mc_error'][chain.rank]=chain.mc_error#batch meansestimate http://arxiv.org/abs/math/0601446
-            self.chains['weight'][chain.rank]=chain.weight
-            self.chains['lnP_max'][chain.rank]=chain.lnP_max
-            self.chains['params_bf'][chain.rank]=chain.params_bf
+    #Update the global state based on the info in local states (dictionary)
+    def update(self,chain_updates):
+        #Loop over chains in update
+        for chain_update in chain_updates:
+            for key in self.chain_keys:
+                self.chains[key][chain.rank]=chain_update[key]
             
             #Update global lnP_max and best fit point
             if chain.lnP_max>self.lnP_max:
-                self.lnP_max=chain.lnP_max
+                self.lnP_bf=chain.lnP_bf
                 self.params_bf=chain.params_bf
     
         
         #Update Global statistics
         self.size=sp.sum(self.chains['size'])
-        self.accepted=sp.sum(self.chains['size'])
+        self.accept=sp.sum(self.chains['accept'])
+        self.weight=sp.sum(self.chains['weight'])
         for key in self.param_names:
-            self.mean[key]=sp.sum(self.chains['weight']*self.chains['mean'][key])/sp.sum(self.chains['mean'][key])
-            self.std[key]=sp.sum()
+            #combine chain means
+            self.mean[key]=sp.sum(self.chains['weight']*self.chains['mean'][key])/float(self.weight)
+            #Add mc errors in (quadrature assuming no correlation)
+            self.mc_error[key]=sp.sqrt(sp.sum(self.chains['weight']*self.chains['mc_error'])/self.weight)
     
-        #print 'Inside update: %i of %i'%(self.sample_size(),self.full_size)
+        #check sampling state: terminate if total size exeeds requested sample size or all chains done
         if self.size>=self.sample_size or not self.chains['continue_sampling'].any():
             #print 'stop_sampling'
             self.continue_sampling=False
@@ -100,86 +75,68 @@ class chain(object):
         self.rank=rank
         self.param_names=model.param_names
         self.batch_size=opt.batch_size#Number of likelihood evaluations per batch
-        self.sample_size=opt.sample_size#Maximum number of likelihood evaluations
+        self.sample_size=opt.sample_size#Maximum number of likelihood evaluations 
         
         #Local properties of chain
-        self.continue_sampling=True
-        self.send=False#wether chain shall send status to master
+        self.continue_sampling=True#Local state of sampling
+        self.send=False#wether chain should send status to master
         self.size=0#Number of Likelihood evaluations
-        self.weight=0#Sum of weights for full chain
-        self.accepted=0#Number of accepted models
-        self.lnP_max=-sp.inf#Local best fit likelihood
-        self.params_bf=None#Local best fit parameter
+        self.accept=0#Number of accepted models
         self.mean=dict([(key,sp.nan) for key in self.params_names])#chain mean
-        self.mc_error=dict([(key,sp.nan) for key in self.params_names])#batch means estimate http://arxiv.org/abs/math/0601446
+        self.mc_error=dict([(key,sp.nan) for key in self.params_names])#batch means estimate
+        self.weight=0#Sum of weights for full chain
+        self.lnP_bf=-sp.inf#Local best fit likelihood
+        self.params_bf=None#Local best fit parameter
+        self.updates=dict([(key,sp.nan) for key in state.chain_keys])#Attributes to be sent to state
+
         #Properties of current batch
-        self.batch={'data':dict([(key,sp.array([])) for key in self.param_names])}#batch data (only accepted points are stored)
-        self.batch['data']['weight']=sp.array([])#weights of data points
-        self.batch['means']=dict([(key,sp.array([])) for key in self.param_names])#list of batch means used to find mc_error
-        self.batch['batch_weight']=dict([(key,sp.array([])) for key in self.param_names])#weights corresponding to params
-        
-        
-        def mean        
-        
-        #calculate mc_error
-        def mc_error(mu,weights):
-            mu_tot=mean(mu,weights)
-            V_1=sp.sum(weights)
-            V_2=sp.sum(weights**2)
-            sigma_2=V_1/float(V_1**2-V_2)*sp.sum(weights*(mu_b-mu_tot)**2)
-            #print sp.sum(weights), len(weights)
-            mc_error=sp.sqrt(sigma_2/float(len(weights)))
-            return mc_error,mu_tot
-    #Method for estimating MCMC error
-    #NB! This method needs to be checked!
-#    def update_stats(self,batch_weight,batch_sum):                
-#        #First time, avoid add to nan error
-#        #print 'pre:',self.mean 
-#        if sp.isnan(self.mean).all():
-#            self.mean=self.batch_sum/float(self.batch_weight)
-#        else:
-#            self.mean=(self.weight*self.mean+batch_sum)/float(self.weight+batch_weight)
-#        #print 'post:',self.mean
-#        self.batch_means+=[batch_sum/float(batch_weight)]
-#        self.weight+=batch_weight
-#        
-#        if len(self.batch_means)>1:
-#            self.mean_error=sp.sqrt((self.batch_size/(len(self.batch_means)-1)*sp.sum((self.batch_means-self.mean)**2,0)))/float(sp.sqrt(len(self.batch_means)*self.batch_size))
-            
+        self.batch={'data':dict([(key,sp.array([])) for key in self.param_names])}#data in current batch (only models with non-zero weight stored (accepted))
+        self.batch['data']['weight']=sp.array([])#weights of points in current batch (needed for statistics)
+        self.batch['mean']=dict([(key,sp.array([])) for key in self.param_names])#batch means
+        self.batch['weight']=dict([(key,sp.array([])) for key in self.param_names])#batch weights
+     
+    #Update chain info (every loop)       
     def update(self,X):
         #Initialize batch after update
         if self.size%self.batch_size==0 and self.size>0:
             self.send=False
+            self.updates=dict([(key,sp.nan) for key in state.chain_keys])
             self.batch['params']=dict([(key,sp.array([])) for key in self.param_names])#reset dictionary
             self.batch['weight']=sp.array([])
          
         #Update number of likelihood evaluations
         self.size+=1
-        
-        #Only add accepted points to batch data (rejected models always have 0 weight)
+        #if accepted update accept
         if X.accept:
-            sp.append(self.batch['weight'],X.weight)
+            self.accept+=1
+            #check best fit point
+            if X.lnP>self.lnP_bf:
+                self.lnP_bf=X.lnP
+                self.params_bf=X.params
+        
+            #add accepted points to batch data (rejected models always have 0 weight)
+            sp.append(self.batch['data']['weight'],X.weight)
             for key in self.batch.keys():
-                sp.append(self.batch['params'][key],X.params[key])
-                sp.append(self.batch['weight'],X.params[key])
+                sp.append(self.batch['data']['params'][key],X.params[key])
                     
         #Update at each batch length
         if self.size%self.batch_size==0 and self.size>0:
-            #check for accepted points in previous batch
-            if len(self.batch.values()[0])>0:
+            #only send if batch check for accepted points in previous batch
+            if len(self.batch['data']['weight'])>0:
+                #add batch weight to array of batch weights
+                sp.append(self.batch['weight'],sp.sum(self.batch['data']['weight']))
+                #add batch mean to array of batch means
                 for key in self.params_names:
-                    self.batch['weight'][key]=self.batch['weight']*self.batch['data'][key]/float(sp.sum(self.batch['weight']))
-                    
-                self.send=True#Set send status to true
+                    sp.append(self.batch['mean'][key],stat.weighted_mean(self.batch['data'][key],self.batch['data']['weight']))
+                    #calculate curren chain mean
+                    self.mean[key]=stat.weighted_mean(self.batch['mean'],self.batch['weight'])
+                    #Calculate current chain mc_error
+                    self.mc_error[key]=stat.mc_error(self.mean,self.batch['mean'][key],self.batch['weight'])
                 
-                
-                
-            
-            if self.batch_weight>0:
-                self.update_stats(self.batch_weight,self.batch_sum)
-                [self.batch_weight,self.batch_sum]=[0,sp.zeros(len(self.param_names))]#Initialize sums for calculaing batch mean
-            #Send results to master
-            
+                #Fill updates for sending
+                self.updates=dict([(key,getattr(self,key)) for key in state.chain_keys])
+                #Set send status to true    
+                self.send=True
 
         if self.size>=self.sample_size:#Final sample size requested
             print 'Worker %i has reached max size, sampling stopped'%(self.rank)
--- lib/stat_lib.py
+++ lib/stat_lib.py
@@ -36,19 +36,10 @@ def weighted_cov(data,weight,mean):
 
     return cov
 
-#calculate mc_error
-def mc_error_rand(batch_means,batch_weights,alg='rand'):
-    total_mean=weighted_mean(batch_means,batch_weights)
-    if alg=='rand':
-        V_1=sp.sum(batch_weights)
-        V_2=sp.sum(batch_weights**2)
-        sigma_2=V_1/float(V_1**2-V_2)*sp.sum(batch_weights*(batch_means-total_mean)**2)
-        #print sp.sum(weights), len(weights)
-        mc_error=sp.sqrt(sigma_2/float(len(batch_weights)))
-    #mcmc requires same size batches
-    if alg=='mcmc':
-        batch_size=batch_weights#integer: All batches have an equal number of models
-        n_batches=len(batch_means)#number of batches
-        mc_error=batch_size/float(n_batches-1)*sp.sum((batch_means-total_mean)**2)#mcmc error according to http://arxiv.org/abs/math/0601446      
-        
-    return mc_error,mu_tot
+#standard mc_error for weighted batches (tested for uniform sampling on gaussian distribution)
+def mc_error(total_mean,batch_means,batch_weights):
+    V_1=sp.sum(batch_weights)
+    V_2=sp.sum(batch_weights**2)
+    sigma_2=V_1/float(V_1**2-V_2)*sp.sum(batch_weights*(batch_means-total_mean)**2)
+    mc_error=sp.sqrt(sigma_2/float(len(batch_weights)))         
+    return mc_error,total_mean
